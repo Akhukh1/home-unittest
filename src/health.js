a4c46de544d29d17e91f5aa4e320eaa1f7f3aecc
Aleks Khukh <aleks.khukh@gmail.com>
2023-08-23 11:42:38 +0300

    Исправления

--- src/health.js
+++ src/health.js
@@ -1,6 +1,10 @@
 export default function healthType(gamer) {
   let result = '';
-  if (gamer.health > 50 && gamer.health < 100) {
+  if (gamer.health < 15) {
+    result = 'critical';
+  } else if (gamer.health < 50) {
+    result = 'wounded';
+  } else if (gamer.health < 100) {
     result = 'healthy';
   }
   return result;
--- test/test.js
+++ test/test.js
@@ -1,7 +1,19 @@
 import healthType from '../src/health';
 
-test('health type', () => {
+test('health type healthy', () => {
   const gamer = { name: 'Маг', health: 90 };
   const result = healthType(gamer);
   expect(result).toEqual('healthy');
+});
+
+test('health type wounded', () => {
+  const gamer = { name: 'лучник', health: 30 };
+  const result = healthType(gamer);
+  expect(result).toEqual('wounded');
+});
+
+test('health type critical', () => {
+  const gamer = { name: 'мечник', health: 10 };
+  const result = healthType(gamer);
+  expect(result).toEqual('critical');
 });
